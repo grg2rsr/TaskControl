534b442497592d62acb55e4b315d64e93b1c27be
grg2rsr <grg2rsr@gmail.com>
2020-02-18 11:05:10 +0000

    taskvis_pg sessionDf dev started

--- TaskVis_pg.py
+++ TaskVis_pg.py
@@ -40,13 +40,111 @@ update_figure()
 event and span vis
 
 
-matplotlib in qr5
+matplotlib in qt5
 https://matplotlib.org/gallery/user_interfaces/embedding_in_qt_sgskip.html
 
 plotting with pyqtgraph in qt5 widgets
 https://www.learnpyqt.com/courses/graphics-plotting/plotting-pyqtgraph/
 
 """
+
+
+"""
+the entire rewrite, again:
+goal: a widget where you can select "advance row on" and "align on"
+eeeh those are the same???
+more about this:
+each row starts with a trial init signal (could never really need something else than trials on y)
+then, how to plot stuff that is in the future if alignment event has not occured in a given trial?
+this will make the past difficult 
+
+one solution: only trigger plot when trial when the next one is active
+this solves the entire past stuff as well
+
+pro: elegant coding: could collect all signals between two trial_available signals
+and do the alignment etc
+less calls to pg.draw() or however its called
+
+
+con: not real time interactivity plotting
+(do we really really need this?) 
+
+then the plot below
+keep a counter, each time advance row on is read on serial, increment
+on each EVENT, plot a bar
+on each _ON record entry (dict last_entires)
+on each _OFF, look for previous _ON and plot a box
+thats all.
+
+two windows:
+session overview (the one described above)
+and a 
+
+session summary view
+this would have trial rate
+%successful trials vs trial number
+number of trial vs time
+(this is until now generalizable)
+
+needs
+SessionDf
+2 kind of parameters:
+
+trial based
+shared
+trial number
+entry time
+
+which kind of trial
+opto
+requested fixation time
+
+
+performance parameter 
+completed/arborted
+such as fixation successful
+reward collected
+reaction time to reward cue (plt ylim based on percentile)
+trial init time
+these could almost be seperate dfs or finally use hierarchical indexing
+
+how to turn these into plots in a generelizable way 
+
+generation
+on each trial init comput this on the previous trial
+"""
+
+
+class SessionVis(QtWidgets.QWidget):
+    def __init__(self, parent):
+        super(SessionVis, self).__init__(parent=parent, code_map=None)
+        self.code_map = code_map
+        self.SessionsDf = pd.DataFrame()
+        self.lines = []
+
+    def initUI(self):
+        pass
+
+    def update(self,line):
+         # if decodeable
+        if not line.startswith('<'):
+            code,t = line.split('\t')
+            t = float(t)
+        else:
+            pass
+
+        if code == "TRIAL_ENTRY_EVENT":
+            # parse lines
+
+            # shared:
+            number = 
+            entry_time = t
+            successful = 
+
+            self.SessionsDf = self.SessionsDf.append()
+
+
+
 
 
 
@@ -59,11 +157,27 @@ class TaskVis(QtWidgets.QWidget):
         self.setWindowFlags(QtCore.Qt.Window)
 
         # connecting signals
+        """ think about whether the children should connect themselves to signals of the parent
+        or whether inside the parents the connections should be set
+        and call and update(line) method of this object ... 
+        pro
+        reduces clutter in this plotter
+        clutter = weird parent().Signals blabla
+        therefore also makes this object more standalone and more portable
+
+        con
+        clutter does not need to be reduced really
+        this object will ever only live here so it can be hard specified
+        portability not really required?
+
+        """
         self.parent().ArduinoController.Signals.serial_data_available.connect(self.on_new_data)
 
         # setting up data structures
+        """ this could be entirely non necessary """
         self.Data = pd.DataFrame(columns=['code','t','name'])
 
+        """ code map should be passed as a constructor parameter ... """
         pio_folder = self.parent().task_config['Arduino']['pio_project_folder']
         event_codes_fname = self.parent().task_config['Arduino']['event_codes_fname']
         code_map_path = self.parent().ArduinoController.task_folder.joinpath(pio_folder,"src",event_codes_fname)
@@ -73,6 +187,9 @@ class TaskVis(QtWidgets.QWidget):
         self.Code_Map = functions.parse_code_map(code_map_path)
         self.code_dict = dict(zip(self.Code_Map['code'].values, self.Code_Map['name'].values))
 
+
+
+        """ these can be moved to init UI, made non self and done ... """
         # the names of the things present in the log
         self.span_names = [name.split('_ON')[0] for name in self.Code_Map['name'] if name.endswith('_ON')]
         self.event_names = [name.split('_EVENT')[0] for name in self.Code_Map['name'] if name.endswith('_EVENT')]
@@ -112,19 +229,46 @@ class TaskVis(QtWidgets.QWidget):
         if not line.startswith('<'):
             code,t = line.split('\t')
             t = float(t)
-            decoded = self.code_dict[code]
-            self.Data = self.Data.append(dict(code=code,t=t,name=decoded),ignore_index=True)
+        else:
+            pass
+
+        decoded = self.code_dict[code]
+
+        if decoded == next_row_event:
+            self.current_row += 1
+
+        if decoded.endswith('_EVENT'):
+            # easy
+
+        if decoded.endswith('_ON'):
+            last_entry[decoded] = t 
+        
+        if decoded.endswith('_OFF':):
+            # add rect from 
+            last_entry[decoded],t
+
+            t_on, t_off = prev['t'], self.Data.iloc[-1]['t']
+            rect = pg.QtGui.QGraphicsRectItem(t_on, self.current_row ,t_off-t_on,1)
+
+            rect.setPen(pg.mkPen(cdict[span_name]))
+            rect.setBrush(pg.mkPen(cdict[span_name]))
+            self.PlotWindow.addItem(rect)
+
+
+            # D = dict(code=code,t=float(t),name=self.code_dict[code])
+            # self.Data = self.Data.append(D,ignore_index=True)
+            """ there is not really a need for keeping this entire dataframe? """
 
             # on the beginning of each trial
-            if self.code_dict[code] == "TRIAL_ENTRY":
-                self.current_trial_entry_time = t
-                self.current_trial_idx += 1
+            # if self.code_dict[code] == "TRIAL_ENTRY":
+            #     self.current_trial_entry_time = t
+            #     self.current_trial_idx += 1
 
             # update plot
-            self.update_plot()
+            # self.update_plot()
         pass
 
-    def update_plot(self):
+    # def update_plot(self):
         # utils.debug_trace()
         # self.TrialLine = self.PlotItem.plot(x=sp.arange(300), y=[self.current_trial_idx,self.current_trial_idx], pen=(200,200,200))
         # if last line is an event
@@ -168,142 +312,3 @@ class TaskVis(QtWidgets.QWidget):
     def closeEvent(self, event):
         # stub
         self.close()
-
-
-# class MyMplCanvas(FigureCanvas):
-#     # FIXME add controls
-#     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-#     # def __init__(self, parent, width=7, height=9, dpi=100):
-#     #     # super(MyMplCanvas, self).__init__(parent=parent)
-#     #     self.parent=parent
-
-#     #     self.lines = []
-
-#     #     # figure init
-#     #     self.fig = Figure(figsize=(width, height), dpi=dpi)
-#     #     self.axes = self.fig.add_subplot(111)
-#     #     FigureCanvas.__init__(self, self.fig)
-#     #     # self.setParent(parent)
-
-#     #     FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-#     #     FigureCanvas.updateGeometry(self)
-
-#     #     self.parent.ArduinoController.Signals.serial_data_available.connect(self.update_data)
-#     #     self.init()
-
-#     #     self.show()
-
-#     def __init__(self, parent, width=7, height=9, dpi=100):
-#         # super(MyMplCanvas, self).__init__(parent=parent)
-#         self.parent=parent
-
-#         self.lines = []
-
-#         # figure init
-#         self.fig = Figure(figsize=(width, height), dpi=dpi)
-#         self.axes = self.fig.add_subplot(111)
-#         FigureCanvas.__init__(self, self.fig)
-#         # self.setParent(parent)
-
-#         FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-#         FigureCanvas.updateGeometry(self)
-
-#         self.parent.ArduinoController.Signals.serial_data_available.connect(self.update_data)
-#         self.init()
-
-#         self.show()
-
-#     def init(self):
-#         """ can only be called if all is running already """
-
-#         pio_folder = self.parent.task_config['Arduino']['pio_project_folder']
-#         event_codes_fname = self.parent.task_config['Arduino']['event_codes_fname']
-#         code_map_path = self.parent.ArduinoController.task_folder.joinpath(pio_folder,"src",event_codes_fname)
-#         self.log_path = self.parent.ArduinoController.run_folder.joinpath('arduino_log.txt')
-
-#         self.Code_Map = functions.parse_code_map(code_map_path)
-#         self.code_dict = dict(zip(self.Code_Map['code'].values, self.Code_Map['name'].values))
-
-
-#     # def init_figure(self):
-#     #     self.axes.plot(sp.randn(100))
-#     #     plt.show()
-#     #     pass
-
-#     def update_data(self, line):
-#         """ connected to new serial data, when any of the trial finishers, update """ 
-
-#         # if decodeable
-#         if not line.startswith('<'):
-#             code = line.split('\t')[0]
-#             decoded = self.code_dict[code]
-#             line = '\t'.join([decoded,line.split('\t')[1]])
-#             self.lines.append(line)
-
-#             if self.code_dict[code] == "TRIAL_COMPLETED_EVENT" or self.code_dict[code] == "TRIAL_ABORTED_EVENT":
-#                 self.update_plot()
-
-#     def update_plot(self):
-#         # make data for plot
-#         valid_lines = [line.strip().split('\t') for line in self.lines if '\t' in line]
-#         self.Data = pd.DataFrame(valid_lines,columns=['name','t'])
-#         self.Data['t'] = self.Data['t'].astype('float')
-                                
-#         # the names of the things present in the log
-#         span_names = [name.split('_ON')[0] for name in self.Code_Map['name'] if name.endswith('_ON')]
-#         event_names = [name.split('_EVENT')[0] for name in self.Code_Map['name'] if name.endswith('_EVENT')]
-
-#         Spans = vis.log2Spans(self.Data, span_names)
-#         Events = vis.log2Events(self.Data, event_names)
-
-#         # definition of the bounding events
-#         trial_entry = "TRIAL_ENTRY_EVENT"
-#         trial_exit_succ = "TRIAL_COMPLETED_EVENT"
-#         trial_exit_unsucc = "TRIAL_ABORTED_EVENT"
-
-#         TrialsDf = vis.make_TrialsDf(self.Data,trial_entry=trial_entry,
-#                                      trial_exit_succ=trial_exit_succ,
-#                                      trial_exit_unsucc=trial_exit_unsucc)
-
-#         # fig, axes = plt.subplots(figsize=(7,9))
-
-#         colors = sns.color_palette('deep',n_colors=len(event_names)+len(span_names))
-#         cdict = dict(zip(event_names+span_names,colors))
-
-#         pre, post = (-1000,1000) # hardcoded
-
-#         for i, row in TrialsDf.iterrows():
-#             if row['outcome'] == 'succ':
-#                 col = 'black'
-#             else:
-#                 col = 'gray'
-#             self.axes.plot([0,row['dt']],[i,i],lw=3,color=col)
-#             self.axes.axhline(i,linestyle=':',color='black',alpha=0.5,lw=1)
-
-#             # adding events as ticks
-#             for event_name, Df in Events.items():
-#                 times = vis.time_slice(Df, row['t_on']+pre, row['t_off']+post, col='t')['t']
-#                 times = times - row['t_on'] # relative to trial onset
-
-#                 for t in times:
-#                     self.axes.plot([t,t],[i-0.5,i+0.5],lw=3,color=cdict[event_name])
-
-#             # adding spans
-#             for span_name, Df in Spans.items():
-#                 Df_sliced = vis.time_slice(Df, row['t_on']+pre, row['t_off']+post, col='t_on')
-#                 for j, row_s in Df_sliced.iterrows():
-#                     t = row_s['t_on'] - row['t_on']
-#                     dur = row_s['dt']
-#                     rect = plt.Rectangle((t,i-0.5), dur, 1,
-#                                         facecolor=cdict[span_name])
-#                     self.axes.add_patch(rect)
-
-#         for key in cdict.keys():
-#             self.axes.plot([0],[0],color=cdict[key],label=key,lw=4)
-#         # self.axes.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',ncol=3, mode="expand", borderaxespad=0.)
-#         # self.axes.invert_yaxis()
-#         self.axes.set_xlabel('time (ms)')
-#         self.axes.set_ylabel('trials')
-#         self.fig.tight_layout()
-#         self.draw()
-        
