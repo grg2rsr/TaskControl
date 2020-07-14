7527b1688af8e013d7bda0a780199a4b560f4965
grg2rsr <grg2rsr@gmail.com>
2020-07-14 10:49:50 +0100

    bonsai harp com extentsion (added but uncommented as not working)

--- BonsaiWidgets.py
+++ BonsaiWidgets.py
@@ -26,8 +26,11 @@ class BonsaiController(QtWidgets.QWidget):
         # constructing the bonsai exe string
         parameters = "-p:save_path=\""+str(save_path)+"\""
 
-        # if 'com_port' in task_config.keys():
-        parameters = parameters+" -p:com_port="+self.config['connections']['firmata_arduino_port']
+        # com port for firmata
+        parameters = parameters + " -p:com_port="+self.config['connections']['firmata_arduino_port']
+
+        # com port for harp
+        # parameters = parameters + " -p:harp_com_port="+self.config['connections']['harp_port']
 
         bonsai_exe = Path(self.config['system']['bonsai_cmd'])
         bonsai_workflow = task_folder / 'Bonsai' / self.task_config['workflow_fname']
