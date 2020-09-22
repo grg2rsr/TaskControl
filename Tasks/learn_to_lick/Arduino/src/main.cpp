2fb18248cc7fc44bf7545431e77a1a23b00db41b
grg2rsr <grg2rsr@gmail.com>
2020-09-22 19:20:54 +0100

    random noise incorrect cue

--- Tasks/learn_to_lick/Arduino/src/main.cpp
+++ Tasks/learn_to_lick/Arduino/src/main.cpp
@@ -86,18 +86,53 @@ void read_lick(){
  ######   #######  ########  ######
 */
 
-bool error_sound_off = true;
-bool play_error_sound = false;
-unsigned long error_sound_on_time = max_future;
-
 void correct_choice_cue(){
     // beep
     tone_controller.play(correct_choice_cue_freq, tone_duration);
 }
 
+/*
+adapted from  https://arduino.stackexchange.com/questions/6715/audio-frequency-white-noise-generation-using-arduino-mini-pro
+*/
+
+unsigned long lastClick = max_future;
+
+/* initialize with any 32 bit non-zero  unsigned long value. */
+#define LFSR_INIT  0xfeedfaceUL
+/* Choose bits 32, 30, 26, 24 from  http://arduino.stackexchange.com/a/6725/6628
+ *  or 32, 22, 2, 1 from 
+ *  http://www.xilinx.com/support/documentation/application_notes/xapp052.pdf
+ *  or bits 32, 16, 3,2  or 0x80010006UL per http://users.ece.cmu.edu/~koopman/lfsr/index.html 
+ *  and http://users.ece.cmu.edu/~koopman/lfsr/32.dat.gz
+ */  
+#define LFSR_MASK  ((unsigned long)( 1UL<<31 | 1UL <<15 | 1UL <<2 | 1UL <<1  ))
+
+unsigned int generateNoise(){ 
+  // See https://en.wikipedia.org/wiki/Linear_feedback_shift_register#Galois_LFSRs
+   static unsigned long int lfsr = LFSR_INIT;  /* 32 bit init, nonzero */
+   /* If the output bit is 1, apply toggle mask.
+                                    * The value has 1 at bits corresponding
+                                    * to taps, 0 elsewhere. */
+
+   if(lfsr & 1) { lfsr =  (lfsr >>1) ^ LFSR_MASK ; return(1);}
+   else         { lfsr >>= 1;                      return(0);}
+}
+
+unsigned long error_cue_start = max_future;
+
 void incorrect_choice_cue(){
     // beep
-    tone_controller.play(incorrect_choice_cue_freq, tone_duration);
+    // tone_controller.play(incorrect_choice_cue_freq, tone_duration);
+
+    // white noise
+    error_cue_start = now();
+    lastClick = micros();
+    while (now() - error_cue_start < tone_duration){
+        if ((micros() - lastClick) > 50 ) { // Changing this value changes the frequency.
+            lastClick = micros();
+            digitalWrite (SPEAKER_PIN, generateNoise());
+        }
+    }
 }
 
 /*
