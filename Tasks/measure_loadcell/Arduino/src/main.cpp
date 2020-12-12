05b998d74cb26bfd372211990946527aeff01ab0
unknown <grg2rsr@gmail.com>
2020-12-12 18:16:37 +0000

    measure loadcell fixes - logging.cpp added

--- Tasks/measure_loadcell/Arduino/src/main.cpp
+++ Tasks/measure_loadcell/Arduino/src/main.cpp
@@ -4,6 +4,7 @@
 #include "interface.cpp"
 #include "raw_interface.cpp"
 #include "pin_map.h"
+#include "logging.cpp"
 
 
 /*
@@ -20,79 +21,6 @@
 int last_state = -1; // whatever other state
 unsigned long max_future = 4294967295; // 2**32 -1
 unsigned long t_state_entry = max_future;
-
-/*
-##        #######   ######    ######   #### ##    ##  ######
-##       ##     ## ##    ##  ##    ##   ##  ###   ## ##    ##
-##       ##     ## ##        ##         ##  ####  ## ##
-##       ##     ## ##   #### ##   ####  ##  ## ## ## ##   ####
-##       ##     ## ##    ##  ##    ##   ##  ##  #### ##    ##
-##       ##     ## ##    ##  ##    ##   ##  ##   ### ##    ##
-########  #######   ######    ######   #### ##    ##  ######
-*/
-
-
-// time
-unsigned long now(){
-    return millis();
-}
-
-// VAR reporters
-char s[128]; // message buffer
-
-void log_bool(const char name[], bool value){
-    if (value==true){
-        snprintf(s, sizeof(s), "<VAR %s %s %lu>", name, "true", now());
-    }
-    else {
-        snprintf(s, sizeof(s), "<VAR %s %s %lu>", name, "false", now());
-    }
-    Serial.println(s);
-}
-
-void log_int(const char name[], int value){
-    snprintf(s, sizeof(s), "<VAR %s %i %lu>", name, value, now());
-    Serial.println(s);
-}
-
-// void log_long(const char name[], long value){
-//     snprintf(s, sizeof(s), "<VAR %s %u %lu>", name, value, now());
-//     Serial.println(s);
-// }
-
-void log_ulong(const char name[], unsigned long value){
-    snprintf(s, sizeof(s), "<VAR %s %lu %lu>", name, value, now());
-    Serial.println(s);
-}
-
-void log_float(const char name[], float value){
-    snprintf(s, sizeof(s), "<VAR %s ", name);
-    Serial.print(s);
-    Serial.print(value);
-    snprintf(s, sizeof(s), " %lu>", now());
-    Serial.println(s);
-}
-
-// specific
-
-void log_code(int code){
-    // Serial.println(String(code) + '\t' + String(now()));
-    snprintf(s, sizeof(s), "%u\t%lu", code, now());
-    Serial.println(s);
-}
-
-void log_msg(const char Message[]){
-    // Serial.println("<MSG " + Message + " "+String(now())+">");
-    snprintf(s, sizeof(s), "<MSG %s %lu>", Message, now());
-    Serial.println(s);
-}
-
-void send_sync_pulse(){
-    // sync w load cell
-    digitalWrite(LC_SYNC_PIN,HIGH);
-    delay(1); // 1 ms unavoidable blindness - TODO delayMicroseconds - test!
-    digitalWrite(LC_SYNC_PIN,LOW);
-}
 
 /*
  ######  ######## ##    ##  ######   #######  ########   ######
@@ -192,9 +120,8 @@ void setup() {
     delay(100);
     Serial.begin(115200); // main serial communication with computer
     Serial1.begin(115200); // serial line for receiving (processed) loadcell X,Y
-    Serial.println("<Arduino is ready to receive commands>");
     delay(1000);
-
+    Serial.println("<Arduino is ready to receive commands>");
 }
 
 void loop() {
