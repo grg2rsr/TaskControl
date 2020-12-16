225551b9094423799244f7bd72eb22689b26c4c8
grg2rsr <grg2rsr@gmail.com>
2020-12-16 12:50:02 +0000

    learn to push v7 dev started

---
+++ Tasks/learn_to_push_v7/Arduino/src/interface_variables.h
@@ -0,0 +1,29 @@
+int correct_choice_cue_freq = 12000;
+unsigned long choice_dur = 10000;
+unsigned long ITI_dur_min = 7000;
+unsigned long ITI_dur_max = 10000;
+unsigned long min_fix_dur = 2000;
+unsigned long reward_magnitude = 10;
+float valve_ul_ms = 0.11240263157894737;
+unsigned long reward_available_dur = 1000;
+
+float X_thresh = 1500.0;
+float X_thresh_start = 1500.0;
+float X_thresh_target = 2500.0;
+float X_thresh_increment = 0.03;
+float X_thresh_decrement = -0.01;
+float XY_fix_box = 750.0;
+
+int correction_loops = 1;
+int corr_loop_entry = 3;
+int corr_loop_exit = 2;
+
+int omit_rewards = 0;
+
+float p_instructed_trial = 1.0;
+float instructed_cue_speed = 0.05;
+int fps = 100;
+
+float bias = 0.5;
+float contrast = 1.0;
+float sigma = 1.0;
