c8e6afac8d1233756796c29530a540fa853015a6
unknown <grg2rsr@gmail.com>
2021-03-22 15:05:38 +0000

    learn to time dev added

---
+++ Tasks/learn_to_time/Arduino/src/interface_variables.h
@@ -0,0 +1,27 @@
+int go_cue_freq = 12000;
+unsigned long tone_dur = 50;
+unsigned long buzz_dur = 50;
+unsigned long trial_entry_buzz_dur = 10;
+unsigned long buzz_left = 165;
+unsigned long buzz_right = 305;
+int led_hsv = 180;
+int led_brightness = 50;
+
+unsigned long ITI_dur_min = 5000;
+unsigned long ITI_dur_max = 7500;
+unsigned long timeout_dur = 2000;
+unsigned long choice_dur = 2000;
+
+int autodeliver_rewards = 0;
+int left_short = 1;
+unsigned long reward_magnitude = 3;
+
+float valve_ul_ms_left = 0.01;
+float valve_ul_ms_right = 0.01;
+
+int correction_loops = 1;
+int corr_loop_entry = 3;
+int corr_loop_exit = 2;
+
+float bias = 0.5;
+float contrast = 1.0;
