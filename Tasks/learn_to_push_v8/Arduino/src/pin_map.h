dc511285e4cadbe374798970b717346ff7e955e2
unknown <grg2rsr@gmail.com>
2021-04-12 11:02:29 +0100

    removing old stuff

--- Tasks/learn_to_push_v8/Arduino/src/pin_map.h
+++
@@ -1,8 +0,0 @@
-const int REWARD_VALVE_PIN = 45;
-const int LICK_PIN = 48;
-const int LC_SYNC_PIN = 50;
-const int CUE_LED_PIN = 15;
-const int LED_PIN = 52;
-const int BUZZ_PIN = 14;
-const int SPEAKER_PIN = 3;
-const int LOOP_PIN = 7;
