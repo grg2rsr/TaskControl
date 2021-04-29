08f11a8bfd71b984969950844be823e9b7937d41
unknown <grg2rsr@gmail.com>
2021-04-29 11:03:14 +0100

    learn to choose added

---
+++ Tasks/learn_to_choose/Arduino/src/interface_variables.h
@@ -0,0 +1,32 @@
+unsigned long tone_dur = 50;
+unsigned long buzz_dur = 50;
+unsigned long trial_entry_buzz_dur = 5;
+unsigned long buzz_center_freq = 235;
+unsigned long buzz_freq_sep = 70;
+int led_hsv = 180;
+int led_brightness = 100;
+
+unsigned long ITI_dur_min = 8500;
+unsigned long ITI_dur_max = 11500;
+unsigned long timeout_dur = 6000;
+unsigned long choice_dur = 2000;
+
+int autodeliver_rewards = 0;
+int left_short = 1;
+unsigned long reward_magnitude = 4;
+
+float valve_ul_ms_left = 0.01;
+float valve_ul_ms_right = 0.01;
+
+int correction_loops = 1;
+int corr_loop_entry = 3;
+int corr_loop_exit = 2;
+unsigned long reach_block_dur = 1000;
+unsigned long min_grasp_dur = 20;
+int trial_autostart = 1;
+float p_timing_trial = 0.0;
+
+float bias = 0.5;
+// float contrast = 1.0;
+
+unsigned long gap = 500;
