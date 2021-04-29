ea6af7e0c4e0ff352a519c164d4e3f38ef5e3b4c
unknown <grg2rsr@gmail.com>
2021-04-29 09:49:37 +0100

    reaching task dev (more files)

---
+++ Tasks/learn_to_fixate_discrete_v1/Arduino/src/interface_variables.h
@@ -0,0 +1,37 @@
+// int go_cue_freq = 12000;
+unsigned long tone_dur = 50;
+unsigned long buzz_dur = 50;
+unsigned long trial_entry_buzz_dur = 5;
+// unsigned long buzz_left = 165;
+// unsigned long buzz_right = 305;
+// unsigned long buzz_lat = 235;
+unsigned long buzz_center_freq = 235;
+unsigned long buzz_freq_sep = 70;
+int led_hsv = 180;
+int led_brightness = 100;
+
+unsigned long ITI_dur_min = 8500;
+unsigned long ITI_dur_max = 11500;
+unsigned long timeout_dur = 6000;
+unsigned long choice_dur = 2000;
+
+int autodeliver_rewards = 0;
+int left_short = 1;
+unsigned long reward_magnitude = 4;
+
+float valve_ul_ms_left = 0.01;
+float valve_ul_ms_right = 0.01;
+
+int lateral_cues = 0;
+int correction_loops = 1;
+int corr_loop_entry = 3;
+int corr_loop_exit = 2;
+unsigned long reach_block_dur = 1000;
+unsigned long min_grasp_dur = 20;
+int trial_autostart = 0;
+float p_timing_trial = 0.1;
+
+// float bias = 0.5;
+// float contrast = 1.0;
+
+unsigned long gap = 500;
---
+++ interface_template_reaching.cpp
@@ -0,0 +1,189 @@
+// basic outline of the reader taken from
+// http://forum.arduino.cc/index.php?topic=396450.0
+
+#include <Arduino.h>
+#include "interface_variables.h"
+
+// this line limits total command length to 128 chars - adjust if necessary (very long var names)
+const byte numChars = 128;
+char receivedChars[numChars];
+char buf[numChars];
+
+boolean newData = false;
+bool verbose = true;
+bool run = false;
+bool deliver_reward_left = false;
+bool deliver_reward_right = false;
+bool present_reward_left_cue = false;
+bool present_reward_right_cue = false;
+bool punish = false;
+
+int current_state = 0; // WATCH OUT this is ini state
+
+// fwd declare functions for logging
+unsigned long now();
+void log_bool(const char name[], bool value);
+void log_int(const char name[], int value);
+// void log_long(const char name[], long value);
+void log_ulong(const char name[], unsigned long value);
+void log_float(const char name[], float value);
+
+void getSerialData() {
+    // check if command data is available and if yes read it
+    // all commands are flanked by <>
+
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+ 
+    // loop that reads the entire command
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        // read until end marker
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        // enter reading if startmarker received
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void processSerialData() {
+    if (newData == true) {
+        // echo back command if verbose
+        if (verbose==true) {
+            snprintf(buf, sizeof(buf), "<Arduino received: %s>", receivedChars);
+            Serial.println(buf);
+        }
+
+        // get total length of message
+        unsigned int len = 0;
+        for (unsigned int i = 0; i < numChars; i++){
+            if (receivedChars[i] == '\0'){
+                len = i;
+                break;
+            }
+        }
+
+        // GET SET CMD 
+        char mode[4];
+        strlcpy(mode, receivedChars, 4);
+
+        // GET
+        if (strcmp(mode,"GET")==0){
+            char varname[len-4+1];
+            strlcpy(varname, receivedChars+4, len-4+1);
+
+        // INSERT_GETTERS
+
+        }
+
+        // SET
+        if (strcmp(mode,"SET")==0){
+            char line[len-4+1];
+            strlcpy(line, receivedChars+4, len-4+1);
+
+            // get index of space
+            len = sizeof(line)/sizeof(char);
+            unsigned int split = 0;
+            for (unsigned int i = 0; i < numChars; i++){
+                if (line[i] == ' '){
+                    split = i;
+                    break;
+                }
+            }
+
+            // split by space
+            char varname[split+1];
+            strlcpy(varname, line, split+1);
+
+            char varvalue[len-split+1];
+            strlcpy(varvalue, line+split+1, len-split+1);
+
+            // INSERT_SETTERS
+
+        }
+
+        // UPD - update trial probs - HARDCODED for now, n trials
+        // format UPD 0 0.031 or similar
+        // if (strcmp(mode,"UPD")==0){
+            
+        //     char line[len-4+1];
+        //     strlcpy(line, receivedChars+4, len-4+1);
+
+        //     // get index of space
+        //     len = sizeof(line)/sizeof(char);
+        //     unsigned int split = 0;
+        //     for (unsigned int i = 0; i < numChars; i++){
+        //         if (line[i] == ' '){
+        //             split = i;
+        //             break;
+        //         }
+        //     }
+
+        //     // split by space
+        //     char varname[split+1];
+        //     strlcpy(varname, line, split+1);
+
+        //     char varvalue[len-split+1];
+        //     strlcpy(varvalue, line+split+1, len-split+1);
+
+        //     int ix = atoi(varname);
+        //     float p = atof(varvalue);
+        //     p_interval[ix] = p;
+        // }
+
+        // CMD
+        if (strcmp(mode,"CMD")==0){
+            char CMD[len-4+1];
+            strlcpy(CMD, receivedChars+4, len-4+1);
+
+            // manually implement functions here
+
+            // Stop and Go functionality
+            if (strcmp(CMD,"RUN")==0){
+                run = true;
+                Serial.println("<Arduino is running>");
+            }
+
+            if (strcmp(CMD,"HALT")==0){
+                run = false;
+                Serial.println("<Arduino is halted>");
+            }
+
+            if (strcmp(CMD,"r")==0){
+                deliver_reward_left = true;
+                present_reward_left_cue = true;
+            }
+
+            if (strcmp(CMD,"t")==0){
+                deliver_reward_right = true;
+                present_reward_right_cue = true;
+            }
+
+            if (strcmp(CMD,"p")==0){
+                punish = true;
+            }
+        }
+
+        newData = false;
+    }
+}
