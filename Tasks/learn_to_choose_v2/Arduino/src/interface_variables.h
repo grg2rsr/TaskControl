b266a1b8c3bee0e8f2e41275766de3421a6a6e28
grg2rsr <grg2rsr@gmail.com>
2021-07-22 12:47:28 +0100

    learn_to_choose_v2: with kamin block and secondary reinforcer on valve

---
+++ Tasks/learn_to_choose_v2/Arduino/src/interface_variables.h
@@ -0,0 +1,38 @@
+unsigned long tone_dur = 50;
+unsigned long tone_freq = 12000;
+unsigned long buzz_dur = 200;
+unsigned long kamin_block_protect_dur = 250;
+unsigned long trial_entry_buzz_dur = 5;
+unsigned long buzz_center_freq = 235;
+unsigned long buzz_freq_sep = 35;
+int led_hsv = 180;
+int led_brightness = 100;
+
+unsigned long ITI_dur_min = 8500;
+unsigned long ITI_dur_max = 11500;
+unsigned long timeout_dur = 6000;
+unsigned long choice_dur = 3000;
+
+int cue_on_rewarded_reach = 0;
+int present_init_cue = 0;
+
+int prob_bias_corr = 0;
+int correction_loops = 1;
+int corr_loop_entry = 3;
+int corr_loop_exit = 2;
+int trial_autostart = 1;
+int autodeliver_rewards = 1;
+
+float miss_frac_thresh = 0.7;
+int n_warmup_trials = 10;
+float p_timing_trial = 0.0;
+
+unsigned long reach_block_dur = 1000;
+unsigned long min_grasp_dur = 20;
+
+int left_short = 1;
+
+unsigned long reward_magnitude = 4;
+float valve_ul_ms_left = 0.01;
+float valve_ul_ms_right = 0.01;
+
